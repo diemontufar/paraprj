5a3c0685305410fa983b1b86096d5148b8c37249
achaves <achaves@student.unimelb.edu.au>
2014-08-19 22:08:09 +1000

    * Added some test class

---
+++ UndeadInfection/src/Agents/Agent.cpp
@@ -0,0 +1,21 @@
+/*
+ * Agent.cpp
+ *
+ *  Created on: 19/08/2014
+ *      Author: achaves
+ */
+
+#include "Agent.h"
+
+namespace agents {
+
+Agent::Agent() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Agent::~Agent() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace agents */
---
+++ UndeadInfection/src/Agents/Agent.h
@@ -0,0 +1,20 @@
+/*
+ * Agent.h
+ *
+ *  Created on: 19/08/2014
+ *      Author: achaves
+ */
+
+#ifndef AGENT_H_
+#define AGENT_H_
+
+namespace agents {
+
+class Agent {
+public:
+	Agent();
+	virtual ~Agent();
+};
+
+} /* namespace agents */
+#endif /* AGENT_H_ */
