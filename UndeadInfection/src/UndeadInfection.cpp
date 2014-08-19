2cad7158fdbd485c89b0755ac040028acbacb363
Diego Montúfar <diogonal0@gmail.com>
2014-08-19 17:31:43 +1000

    First setup

---
+++ UndeadInfection/src/UndeadInfection.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : UndeadInfection.cpp
+// Author      : Diego
+// Version     :
+// Copyright   : copyright 2014
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
