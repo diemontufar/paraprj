a31b21e99f37da0eb1f455b32a56bf88c763b53c
GCarrion03 <gustavocarrion03@hotmail.com>
2014-08-30 19:03:55 +1000

    basic add delete move changes

--- UndeadInfection/src/Model/Grid.cpp
+++ UndeadInfection/src/Model/Grid.cpp
@@ -9,10 +9,10 @@
 #include <iostream>
 using namespace std;
 Grid::Grid() {
-	cells =  new Cell*[CELLROWSPERGRID];
-	for (int i = 0; i < CELLROWSPERGRID; i++){
-		cells[i] =  new Cell[CELLCOLUMNSPERGRID];
-	}
+	//cells =  new Cell[CELLROWSPERGRID];
+	//for (int i = 0; i < CELLROWSPERGRID; i++){
+		//cells[i] =  new Cell[CELLCOLUMNSPERGRID];
+	//}
 
 }
 
@@ -24,15 +24,32 @@ void Grid::initialize(int nPeople, int nZombies){
 	for ( int i = 0; i < CELLROWSPERGRID; i++ ){
 		for ( int j = 0; j < CELLCOLUMNSPERGRID; j++ ){
 			cells[i][j]=new Cell();
-			cells[i][j]->setCurrentAgent(new Human(false,false));
+			cells[i][j]->setGrid(this);
+
+			//todo:change algorithm so it is random and with pop limits
+			if (j<CELLCOLUMNSPERGRID/2){
+				cells[i][j]->setCurrentAgent(new Human(RandomGenerator::randomBool() ,RandomGenerator::randomBool()));
+			}
 			cout << "i:  "<<i << "j:  "<<j <<"Initialized with humans: "<< endl;
 		}
 	}
 }
 
-void Grid::addAgent(int x, int y, Agent* agent){
-	cout << "x:  "<<x << "y:  "<<y<< endl;
-}
-void Grid::moveAgent(int xcurr, int ycurr, Agent* agent){
 
+//Todo: check adjacencies and move
+void Grid::addAgent(int x, int y, Agent* agent){
+	if (cells[x][y]->getCurrentAgent()==nullptr){
+		cells[x][y]->setCurrentAgent(agent);
+	}
+	cout << "addent and agent at: "<<"x:  "<<x << "y:  "<<y<< endl;
+}
+void Grid::moveAgent(int x, int y, Agent* agent,int xtoMove ,int ytoMove){
+	if (cells[xtoMove][ytoMove]->getCurrentAgent()==nullptr){
+			cells[xtoMove][ytoMove]->setCurrentAgent(agent);
+			cells[x][y]->setCurrentAgent(nullptr);
+		}
+		cout << "addent and agent at: "<<"x:  "<<x << "y:  "<<y<< endl;
+}
+void Grid::removeAgent(int x, int y, Agent* agent){
+	cells[x][y]->setCurrentAgent(nullptr);
 }
