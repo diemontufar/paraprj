f723d2924c152bdf90ffe912089f6f22a817bfac
achaves <achaves@student.unimelb.edu.au>
2014-09-13 16:39:35 +1000

    * Assigned real parameters in constant

--- UndeadInfection/src/Constants.h
+++ UndeadInfection/src/Constants.h
@@ -19,35 +19,28 @@
  * Probability weapon: 0.3
  */
 
-static const long NUMTICKS = 3000; //Number of ticks
-static const long YEARTICKS = 3; //Ticks in 1 year
+static const long NUMTICKS = 45625; //Number of ticks 125*365
+static const long YEARTICKS = 365; //Ticks in 1 year 365
 
-static const int HUMANSPEED = 2; //Human speed
 static const int INCUBATIONTIME = 5;//Ticks that take the zombie disease to incubate in an infected human
+static int const GRIDROWS = 1000; //Number of row grids
+static int const GRIDCOLUMNS = 1500;//Number of column grids
 
-static int const GRIDROWS = 40; //Number of row grids
-static int const GRIDCOLUMNS = 40;//Number of column grids
-
-static const int ZOMBIESPEED = 1; //Zombie speed
 static const int MINDECOMPOSITIONTIME = 10;//Ticks that take the zombie to decompose
 static const int MAXDECOMPOSITIONTIME = 15;//Ticks that take the zombie to decompose
 
-static const int MINLIFEEXPECTANCY = 25;//minimum human life expectancy
-//static const int MINLIFEEXPECTANCY = 65*365;//minimum human life expectancy
-static const int MAXLIFEEXPECTANCY= 35;//maximum human life expectancy
-//static const int MAXLIFEEXPECTANCY= 90*365;//maximum human life expectancy
+static const int MINLIFEEXPECTANCY = 65;//minimum human life expectancy
+static const int MAXLIFEEXPECTANCY= 75;//maximum human life expectancy
 
-static const float DARWINPOPDENSITY = 100;
+
 static const float NUMBEROFZOMBIES = 10;
-//static const float DARWINPOPDENSITY = 926/2;
-static const float NTPOPDENSITY = 0.17;
+static const float NTPOPDENSITY = 0.17/2.0;
+static const float DARWINPOPDENSITY = 1000.0*1500.0*NTPOPDENSITY;
+
 static const float GENDERRATIO = 0.483; //Women vs Men
 
 static const float GUNDENSITY = 0.3; //19.13
-static const float BIRTHPERCENTAGE = 0.5;
-//static const float BIRTHPERCENTAGE = 864.0/23000000.0; //
-
-//static const float BIRTHPERCENTAGE = 864.0/23000000.0; //
+static const float BIRTHPERCENTAGE = 864.0/23000000.0; //
 
 
 static const float HEADSHOTPERCENTAGE = 10; //40%
